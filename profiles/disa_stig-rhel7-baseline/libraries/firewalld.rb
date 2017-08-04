14ae6238f09f377bafe40affa317b88b999e6246
dromazos <dromazmj@dukes.jmu.edu>
2017-08-04 09:11:37 -0400

    removes sudo from commands and insterts comment for system assumption

--- profiles/disa_stig-rhel7-baseline/libraries/firewalld.rb
+++ profiles/disa_stig-rhel7-baseline/libraries/firewalld.rb
@@ -1,29 +1,14 @@
-# encoding: utf-8
-#
-=begin
------------------
-Benchmark: Red Hat Enterprise Linux 7 Security Technical Implementation Guide
-Status: Accepted
 
-This Security Technical Implementation Guide is published as a tool to improve
-the security of Department of Defense (DoD) information systems. The
-requirements are derived from the National Institute of Standards and
-Technology (NIST) 800-53 and related documents. Comments or proposed revisions
-to this document should be sent via email to the following address:
-disa.stig_spt@mail.mil.
-
-Release Date: 2017-03-08
-Version: 1
-Publisher: DISA
-Source: STIG.DOD.MIL
-uri: http://iase.disa.mil
------------------
-=end
 require 'pry'
 require 'nokogiri'
 require 'ipaddr'
 
 class FirewallD < Inspec::resource(1)
+  ###
+  #  This recourse assumes that the file sudo vim /etc/polkit-1/rules.d/49-nopasswd_global.rules has been
+  #  set to allow users in group "wheel" to perform any commands without authentication.
+  ###
+
   name 'firewalld'
   desc 'Use the firewalld resource to check and see if firewalld is configured to grand or deny access to specific hosts or services'
   example "
@@ -65,13 +50,13 @@ class FirewallD < Inspec::resource(1)
 
   def zone_exists?(query_zone)
     return false unless exist?
-    result = firewalld_command("sudo firewall-cmd --get-zones").split(" ")
+    result = firewalld_command("firewall-cmd --get-zones").split(" ")
     result.include?(query_zone)
   end
 
   def running?
     return false unless exist?
-    result = firewalld_command("sudo firewall-cmd --state")
+    result = firewalld_command("firewall-cmd --state")
     return result unless result == "running\n" || result == "not running\n"
     result[ 0, result.length-1 ] == "running"
   end
@@ -85,31 +70,31 @@ class FirewallD < Inspec::resource(1)
   end
 
   def service_enabled_in_zone(query_zone=default_zone, query_service)
-    firewalld_command("sudo firewall-cmd --zone=#{query_zone} --query-service=#{query_service}")[ 0 .. -2 ]
+    firewalld_command("firewall-cmd --zone=#{query_zone} --query-service=#{query_service}")[ 0 .. -2 ]
   end
 
   def service_ports_enabled_in_zone(query_zone=default_zone, query_service)
-    firewalld_command("sudo firewall-cmd --zone=#{query_zone} -service=#{query_service} --get-ports")
+    firewalld_command("firewall-cmd --zone=#{query_zone} -service=#{query_service} --get-ports")
   end
 
   def service_protocols_enabled_in_zone(query_zone=default_zone, query_service)
-    firewalld_command("sudo firewall-cmd --zone=#{query_zone} --service=#{query_service} --get-protocols")
+    firewalld_command("firewall-cmd --zone=#{query_zone} --service=#{query_service} --get-protocols")
   end
 
   def port_enabled_in_zone(query_zone=default_zone, query_port)
-    firewalld_command("sudo firewall-cmd --zone=#{query_zone} --query-port=#{query_port}")[ 0 .. -2 ]
+    firewalld_command("firewall-cmd --zone=#{query_zone} --query-port=#{query_port}")[ 0 .. -2 ]
   end
 
   def sources_bound(query_zone=default_zone)
-    firewalld_command("sudo firewall-cmd --zone=#{query_zone} --list-sources")
+    firewalld_command("firewall-cmd --zone=#{query_zone} --list-sources")
   end
 
   def services_bound(query_zone=default_zone)
-    firewalld_command("sudo firewall-cmd --zone=#{query_zone} --list-services")
+    firewalld_command("firewall-cmd --zone=#{query_zone} --list-services")
   end
 
   def rule_enabled(rule, query_zone=default_zone)
-    firewalld_command("sudo firewall-cmd --zone=#{default_zone} --query-rich-rule='#{rule}'")[ 0 .. -2 ]
+    firewalld_command("firewall-cmd --zone=#{default_zone} --query-rich-rule='#{rule}'")[ 0 .. -2 ]
   end
 
   private
