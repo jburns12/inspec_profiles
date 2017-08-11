63c00314d43028aaea51e09a504e98a6d9b01795
Jennifer Burns <jburns@mitre.org>
2017-08-11 09:00:26 -0400

    Updated aide_conf to prepare for PR

--- profiles/disa_stig-rhel7-baseline/libraries/aide_conf.rb
+++ profiles/disa_stig-rhel7-baseline/libraries/aide_conf.rb
@@ -20,7 +20,6 @@ class AideConf < Inspec::resource(1)
   def initialize(aide_conf_path = nil)
     return skip_resource 'The `aide_conf` resource is not supported on your OS.' unless inspec.os.linux?
     @conf_path = aide_conf_path || '/etc/aide.conf'
-    @files_contents = {}
     @content = nil
     @rules = nil
     read_content
@@ -34,9 +33,10 @@ class AideConf < Inspec::resource(1)
     in_all_lines = true
     all_lines = parse_conf(@content)
     all_lines.each do |line|
-      if line['selection_line'] != nil then
-        if !line['rules'].include? "#{rule}" then
+      if line['selection_line'] != nil
+        if !line['rules'].include? "#{rule}"
           in_all_lines = false
+          break
         end
       end
     end
@@ -53,6 +53,18 @@ class AideConf < Inspec::resource(1)
 
   private
 
+  def read_content
+    @content = ''
+    @rules = {}
+    @content = read_file(@conf_path)
+    # Return if @content consists of skip_resource message
+    if @content.instance_of?(String)
+      return @content
+    end
+    @content = filter_comments(@content)
+    @params = parse_conf(@content)
+  end
+
   def filter_comments(data)
     content = []
     data.each do |line|
@@ -62,17 +74,6 @@ class AideConf < Inspec::resource(1)
     content
   end
 
-  def read_content
-    @content = ''
-    @rules = {}
-    @content = read_file(@conf_path)
-    if @content.instance_of?(String)
-      return @content
-    end
-    @content = filter_comments(@content)
-    @params = parse_conf(@content)
-  end
-
   def parse_conf(content)
     content.map do |line|
       parse_line(line)
@@ -80,62 +81,66 @@ class AideConf < Inspec::resource(1)
   end
 
   def parse_line(line)
-    selection_line = nil
-    rule_list = nil
+    line_and_rules = {}
+    # Case when line is a rule line
+    if line.include? " = "
+      parse_rule_line(line)
+    end
+    # Case when line is a selection line
+    if line.start_with?('/')
+      line_and_rules = parse_selection_line(line)
+    end
+    line_and_rules
+  end
+
+  def parse_rule_line(line)
+    line.gsub!(/\s+/, "")
+    rule_line_arr = line.split("=")
+    rules_list = rule_line_arr.last.split("+")
+    rule_name = rule_line_arr.first
+    rules_list.each_index do |i|
+      # Cases where rule respresents one or more other rules
+      if @rules.key?("#{rules_list[i]}")
+        rules_list[i] = @rules["#{rules_list[i]}"]
+      end
+      check_multi_rule(rules_list, i)
+    end
+    @rules["#{rule_name}"] = rules_list.flatten
+  end
+
+  def parse_selection_line(line)
+    selec_line_arr = line.split(" ")
+    selection_line = selec_line_arr.first
+    rule_list = selec_line_arr.last.split("+")
+    rule_list.each_index do |i|
+      hash_list = @rules["#{rule_list[i]}"]
+      # Cases where rule respresents one or more other rules
+      if hash_list != nil
+        rule_list[i] = hash_list
+      end
+      check_multi_rule(rule_list, i)
+    end
+    rule_list.flatten!
+    {
+      'selection_line' => selection_line,
+      'rules' => rule_list,
+    }
+  end
+
+  def check_multi_rule(rule_list, i)
     # Rules that represent multiple rules (R,L,>)
     r_rules = ['p', 'i', 'l', 'n', 'u', 'g', 's', 'm', 'c', 'md5']
     l_rules = ['p', 'i', 'l', 'n', 'u', 'g']
     grow_log_rules = ['p', 'l', 'u', 'g', 'i', 'n', 'S']
 
-    # Case when line is a rule line
-    if line.include? " = " then
-      line.gsub!(/\s+/, "")
-      rule_line_arr = line.split("=")
-      rules_list = rule_line_arr.last.split("+")
-      rule_name = rule_line_arr.first
-      rules_list.each_index do |i|
-        # Cases where rule respresents one or more other rules
-        if @rules.key?("#{rules_list[i]}")
-          rules_list[i] = @rules["#{rules_list[i]}"]
-        end
-        case rules_list[i]
-        when "R"
-          rules_list[i] = r_rules
-        when "L"
-          rules_list[i] = l_rules
-        when ">"
-          rules_list[i] = grow_log_rules
-        end
-      end
-      @rules["#{rule_name}"] = rules_list.flatten
+    case rule_list[i]
+    when "R"
+      rule_list[i] = r_rules
+    when "L"
+      rule_list[i] = l_rules
+    when ">"
+      rule_list[i] = grow_log_rules
     end
-
-    # Case when line is a selection line
-    if line.start_with?('/')
-      selec_line_arr = line.split(" ")
-      selection_line = selec_line_arr.first
-      rule_list = selec_line_arr.last.split("+")
-      rule_list.each_index do |i|
-        hash_list = @rules["#{rule_list[i]}"]
-        # Cases where rule respresents one or more other rules
-        if hash_list != nil
-          rule_list[i] = hash_list
-        end
-        case rule_list[i]
-        when "R"
-          rule_list[i] = r_rules
-        when "L"
-          rule_list[i] = l_rules
-        when ">"
-          rule_list[i] = grow_log_rules
-        end
-      end
-      rule_list.flatten!
-    end
-    {
-      'selection_line' => selection_line,
-      'rules' => rule_list,
-    }
   end
 
   def read_file(conf_path = @conf_path)
